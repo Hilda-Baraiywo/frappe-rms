16ed60c0f0a3e91c2d4f7c7fd6a02708bb4c22dd
baraiywohilda181@gmail.com <Hilda-Baraiywo@github.com>
2024-05-22 08:47:08 +0300

    initial commit

---
+++ restaurant_management_system/restaurant_management_system/doctype/reservation_reminders/reservation_reminders.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Hilda Baraiywo and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Reservation Reminders", {
+// 	refresh(frm) {
+
+// 	},
+// });
